5957cd9fc31e7129ebefc42fef4226e0a8baa8a2
sachin1649 <spsachinonspot@gmail.com>
2016-06-10 15:02:35 +0530

    Change 1.9

--- editproj.php
+++ editproj.php
@@ -1,6 +1,7 @@
 <?php
-include_once('connection.php');
+include('connection.php');
 session_start();
+$pid=$_POST['pid'];
 $manger_id=$_SESSION['manager_id'];
 $proj_name=$_POST['proj_name'];
 $proj_assign=$_POST['assign_to'];
@@ -8,26 +9,12 @@ $proj_mem_id=$_POST['proj_member_id'];
 $proj_desc=$_POST['proj_desc'];
 $proj_dead=$_POST['proj_dead'];
 $proj_miles=$_POST['mile_stone'];
-
-$proj_doc=$_FILES['proj_doco']['name'];
-//$filname=$proj_doco."".$proj_name;
-$proj_temp=$_FILES['proj_doco']['tmp_name'];
-if(is_dir('upload/'.$manger_id))
-	{
-		$destt="upload/".$manger_id."/".$proj_doc;
-		move_uploaded_file($proj_temp, $destt);
-	}
-else  {
-	mkdir('upload/'.$manger_id);
-		$destt="upload/".$manger_id."/".$proj_doc;
-		move_uploaded_file($proj_temp, $destt);
-		}	
-//	echo $destt;
+/*
 $addpro_query="insert into project(`project_name`,`project_manger_id`,`project_member_id`,`assign_to`,`project_desc`,`project_milestone`,`project_document`,`project_end_date`) values('$proj_name','$manger_id','$proj_mem_id','$proj_assign','$proj_desc','$proj_miles','$destt','$proj_dead')";
-$quer=mysqli_query($conn,$addpro_query);
-header('location:projects.php');
+*/
+$quer="update project set project_name='$proj_name', assign_to='$proj_assign',project_manger_id='$proj_mem_id',project_desc='$proj_desc', project_end_date='$proj_dead',
+project_milestone='$proj_miles' where project_id='$pid'";
+$res=mysqli_query($conn,$quer);
 
 
-
-
-?>
+ ?>
